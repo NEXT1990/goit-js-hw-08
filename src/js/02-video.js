7255cd895251824e1556564fa0857e3d4c753684
NEXT1990 <maksim.listunov@gmail.com>
2022-11-28 00:57:52 +0200

    add-time-to-localStorager

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+// player.on('timeupdate', function () {
+//   player.getCurrentTime().then(function (seconds) {
+//     let timeSeconds = seconds;
+//     console.log(timeSeconds);
+//     localStorage.setItem('currentTimeWatchVideo', timeSeconds);
+//   });
+// });
+
+player.on('timeupdate', throttle(currentTime, 1000));
+
+function currentTime(event) {
+  let timeSeconds = event.seconds;
+  console.log(timeSeconds);
+  localStorage.setItem('currentTimeWatchVideo', timeSeconds);
+}
