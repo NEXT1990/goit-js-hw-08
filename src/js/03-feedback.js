b6f1e51748a671bc28fdb09a745498a54b52d4bb
NEXT1990 <maksim.listunov@gmail.com>
2022-11-29 18:23:54 +0200

    add-task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,14 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  inputEmail: document.querySelector('input[type="email"]'),
+  inputMassage: document.querySelector('textarea[name="message"]'),
+};
+
+console.log(refs.form);
+console.log(refs.inputEmail);
+console.log(refs.inputMassage);
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.inputEmail.addEventListener('input', onEmailInput);
