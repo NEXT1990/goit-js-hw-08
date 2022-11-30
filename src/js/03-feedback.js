22d7e74f2b8870779ef6ba635b9dbeda11c7b1fb
NEXT1990 <maksim.listunov@gmail.com>
2022-11-30 03:13:12 +0200

    update-task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,14 +1,35 @@
 import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+
 const refs = {
   form: document.querySelector('.feedback-form'),
   inputEmail: document.querySelector('input[type="email"]'),
-  inputMassage: document.querySelector('textarea[name="message"]'),
+  inputMessage: document.querySelector('textarea[name="message"]'),
 };
 
 console.log(refs.form);
 console.log(refs.inputEmail);
-console.log(refs.inputMassage);
+console.log(refs.inputMessage);
 
 refs.form.addEventListener('submit', onFormSubmit);
-refs.inputEmail.addEventListener('input', onEmailInput);
+refs.form.addEventListener('input', event => {
+  formData[event.target.name] = event.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify({ formData }));
+});
+
+onLocalStorageGetMessage();
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  event.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function onLocalStorageGetMessage() {
+  const savedMessage = localStorage.getItem(STORAGE_KEY);
+  if (savedMessage) {
+    console.log(savedMessage);
+  }
+}
