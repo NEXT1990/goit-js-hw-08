796d875fb12e08626cd2126fe53e2b5a6bf0b1ce
NEXT1990 <maksim.listunov@gmail.com>
2022-12-05 10:10:36 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -21,10 +21,12 @@ refs.form.addEventListener(
     let savedDataLocalStorage = JSON.parse(localStorage.getItem(STORAGE_KEY));
 
     if (savedDataLocalStorage) {
-      console.log(savedDataLocalStorage);
+      // console.log(savedDataLocalStorage);
     }
-    let { email, message } = event.currentTarget.elements;
-    savedDataLocalStorage = { email: email.value, message: message.value };
+    savedDataLocalStorage = {
+      ...savedDataLocalStorage,
+      [event.target.name]: event.target.value,
+    };
     localStorage.setItem(STORAGE_KEY, JSON.stringify(savedDataLocalStorage));
   }, 500)
 );
